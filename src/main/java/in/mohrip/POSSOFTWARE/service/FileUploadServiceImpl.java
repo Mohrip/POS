147f4e77ead3919a5a5eacba1035774b6c1b2c6e
mohrip <m25srt@gmail.com>
2025-09-07 19:24:46 +0300

    add AWS file

--- src/main/java/in/mohrip/POSSOFTWARE/service/FileUploadServiceImpl.java
+++ src/main/java/in/mohrip/POSSOFTWARE/service/FileUploadServiceImpl.java
@@ -1,18 +1,39 @@
 package in.mohrip.POSSOFTWARE.service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import in.mohrip.POSSOFTWARE.service.FileUploadService;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class FileUploadServiceImpl implements FileUploadService {
 
     @Override
-    public String uploadFile(org.springframework.web.multipart.MultipartFile file) {
+    public String uploadFile(MultipartFile file) {
         // TODO Auto-generated method stub
-        return null;
+        String fileNameExtintion = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
+        String key = UUID.randomUUID().toString() + "." + fileNameExtintion;
+        try {
+            PutObjectRequest response = PutObjectRequest.builder()
+                    .bucket("mohrip-pos-software")
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .contentLength(file.getSize())
+                    .build();
+            if(response.sdkFields().indexOf()){
+                return "https://mohrip-pos-software.s3.amazonaws.com/"+key;
+            }else{
+                throw new RsponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "File upload failed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // return null;
+        }
     }
-
     @Override
     public boolean deleteFile(String imageUrl) {
         // TODO Auto-generated method stub
